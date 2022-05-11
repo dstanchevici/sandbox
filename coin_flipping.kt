376600cbb931e786c90e32f4e15e63074df78727
Dmitri Stanchevici <distanchev@gmail.com>
2022-05-11 11:16:52 -0400

    Added coin_flipping
    description

---
+++ coin_flipping.kt
@@ -0,0 +1,8 @@
+package sandbox
+
+// Sedgewick Program 3.2 Coin-Flipping Simulator
+
+// The program runs the experiment of executing n coin flips. This experiment is run m times.
+// An array keeps track of the frequency of occurrence of the outcome "i heads" for 0<=i<=n.
+// A histogram is printed of the result of the experiments, with one asterisk for each 10 occurrences.
+
